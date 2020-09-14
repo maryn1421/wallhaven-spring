1c8fc259832925237a548f05b3f2b03409c8fbfd
maryn1421 <zolnaimaryn1421@gmail.com>
2020-09-14 15:09:17 +0200

    Initial commit

---
+++ src/main/java/com/codecool/wallhaven/WallhavenSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.wallhaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallhavenSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WallhavenSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/wallhaven/WallhavenSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.wallhaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallhavenSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
