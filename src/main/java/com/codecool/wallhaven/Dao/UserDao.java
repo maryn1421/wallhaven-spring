70a6518ca769bd359928d2cfba19da942c0b2870
Maryn Zolnai <zolnaimaryn1421@gmail.com>
2020-09-16 17:45:05 +0200

    finished add friend

--- src/main/java/com/codecool/wallhaven/Dao/UserDao.java
+++ src/main/java/com/codecool/wallhaven/Dao/UserDao.java
@@ -6,12 +6,16 @@ import java.util.List;
 
 public interface UserDao {
     void addUser( User user);
+    void addFriend(int userId, int friendId);
     User getUSer();
     void deleteUser(User user);
     void updateUser(User uSer);
 
     List<User> getAllUser();
-
+    List<User> getFriendsById(int userId);
 
     boolean checkLogin(String email, String password);
+
+    User getUserById(int id);
+    String getIdByEmail(String email);
 }
--- src/main/java/com/codecool/wallhaven/Dao/implementation/UserDaoJDBC.java
+++ src/main/java/com/codecool/wallhaven/Dao/implementation/UserDaoJDBC.java
@@ -5,7 +5,10 @@ import com.codecool.wallhaven.model.User;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserDaoJDBC implements UserDao {
     private DataSource dataSource;
@@ -14,6 +17,72 @@ public class UserDaoJDBC implements UserDao {
         this.dataSource = dataSource;
 
     }
+    @Override
+    public List<User> getFriendsById(int userid) {
+        List<User> friends = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "SELECT friends FROM users WHERE id = ?";
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, userid);
+            ResultSet resultSet = statement.executeQuery();
+            if (!resultSet.next()) {
+                return null;
+            }
+            Array array = resultSet.getArray(1);
+            Integer[] ints = (Integer[]) array.getArray();
+            List<Integer> friendIds = Arrays.asList(ints);
+
+            friendIds.forEach(id -> {
+                User user = getUserById(id);
+                if (user != null) {
+                    friends.add(user);
+                }
+            });
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return friends;
+    }
+
+    @Override
+    public User getUserById(int id) {
+        User user = new User("asd", "asd", "asd");
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "SELECT username, password, email FROM users WHERE id = ?";
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (!resultSet.next()) {
+                return null;
+            }
+            user = new User(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3));
+            user.setId(id);
+            return user;
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return user;
+    }
+
+    @Override
+    public String getIdByEmail(String email) {
+        int userId = 0;
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "SELECT id FROM users WHERE email = ?";
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, email);
+            ResultSet resultSet = statement.executeQuery();
+            if (!resultSet.next()) {
+                return email;
+            }
+            userId = resultSet.getInt(1);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return Integer.toString(userId);
+    }
+
 
     @Override
     public void addUser(User user) {
@@ -31,6 +100,29 @@ public class UserDaoJDBC implements UserDao {
             user.setId(resultSet.getInt(1));
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void addFriend(int userId, int friendId) {
+        List<User> actualFriends = getFriendsById(userId);
+        List<Integer> ids = new ArrayList<>();
+        actualFriends.forEach(friend -> {
+            ids.add(friend.getId());
+        });
+        ids.add(friendId);
+        List<Integer> newIds = ids.stream()
+                .distinct()
+                .collect(Collectors.toList());
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "UPDATE users SET friends = ? WHERE id = ? ";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            Array array = connection.createArrayOf("Integer", newIds.toArray());
+            preparedStatement.setArray(1, array);
+            preparedStatement.setInt(2, userId);
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
     }
 
@@ -71,6 +163,23 @@ public class UserDaoJDBC implements UserDao {
 
     @Override
     public List<User> getAllUser() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "SELECT id, username, password, email FROM users";
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ResultSet resultSet = statement.executeQuery();
+            if (!resultSet.next()) {
+                return null;
+            }
+            while (resultSet.next()) {
+                User user = new User(resultSet.getString(2), resultSet.getString(3), resultSet.getString(4));
+                user.setId(resultSet.getInt(1));
+                users.add(user);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return users;
     }
 }
