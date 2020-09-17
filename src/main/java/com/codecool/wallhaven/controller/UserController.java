3dbd9d0f2f6cbc368a0e5161f34f7549e930111c
Maryn Zolnai <zolnaimaryn1421@gmail.com>
2020-09-17 11:48:37 +0200

    added new branch

--- src/main/java/com/codecool/wallhaven/controller/UserController.java
+++ src/main/java/com/codecool/wallhaven/controller/UserController.java
@@ -8,6 +8,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+//nb
+
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserController {
