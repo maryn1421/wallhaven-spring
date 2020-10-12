e0b690321e04ddb78c8e12ba47bbb8c1b1d73fa1
maryn1421 <zolnaimaryn1421@gmail.com>
2020-10-12 17:47:03 +0200

    edited application.properties, added secret key and expiration time

--- src/main/resources/sample_data/roles.sql
+++ src/main/resources/sample_data/roles.sql
@@ -1,3 +1,4 @@
-INSERT INTO roles(name) VALUES('ROLE_USER');
-INSERT INTO roles(name) VALUES('ROLE_MODERATOR');
-INSERT INTO roles(name) VALUES('ROLE_ADMIN');
+INSERT INTO roles(name)
+VALUES ('ROLE_USER'),
+       ('ROLE_MODERATOR'),
+       ('ROLE_ADMIN');
